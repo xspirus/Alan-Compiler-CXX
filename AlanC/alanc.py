15b46be61e542fd1a92970becd37ce0d0ac5eb46
xspirus <spirosdontas@gmail.com>
2018-11-19 15:26:52 +0200

    Added --help support

--- AlanC/alanc.py
+++ AlanC/alanc.py
@@ -51,8 +51,37 @@ def makeTempFiles(tempdir, name) :
     tempS = os.path.join(tempdir, name + '.s')
     return tempLL, tempS
 
+def help() :
+    lines = [
+            "ALAN COMPILER v1.0.0",
+            "",
+            "usage: acc [options] file",
+            "",
+            "Options:",
+            "  -o <file>",
+            "    Place the output file to <file>",
+            "    Output files are placed by default in folder \"execs\"",
+            "  -O0",
+            "    Compile with no optimisations",
+            "  -O1",
+            "    Enable basic optimisations",
+            "  -O2",
+            "    Enable more optimisations",
+            "  -O3",
+            "    Enable all optimisations",
+            "  --help",
+            "    Show this menu",
+            ""
+            ]
+    for line in lines :
+        print(line)
+    sys.exit(0)
+# end help
+
 def main(argv) :
     inFile, outFile, extras = args(argv)
+    if ( "--help" in extras ) :
+        help()
     if ( os.path.isdir(TEMP) ) :
         shutil.rmtree(TEMP)
     os.makedirs(TEMP)
